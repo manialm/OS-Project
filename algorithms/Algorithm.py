0c5b3a97e475d4444796bc7d0ac46972e327c2c6
Mani Alimadadi <mani.alimadadi7@gmail.com>
2023-01-22 04:07:29 +0330

    Fix Algorithm

--- algorithms/Algorithm.py
+++ algorithms/Algorithm.py
@@ -1,6 +1,10 @@
 import abc
+from queue import PriorityQueue
 
+from Process import Process
+from SchedulingInfo import SchedulingInfo
 class Algorithm(abc.ABC):
     @abc.abstractmethod
-    def choose_next(self, scheduling_infos, ready_queue):
+    def choose_next(self, scheduling_info: dict[Process, SchedulingInfo],
+    ready_list: list[tuple[int, Process]]) -> tuple[Process, int]
         raise NotImplementedError
